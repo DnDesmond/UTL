bec41830ec88c846fa65be375e63d7085ec9d48b
isaac <isaac.ocarrolldesmond@gmail.com>
2024-09-23 19:34:26 +0200

    Added transition offset for first 2 rooms

--- WhitherThither.tsx
+++ WhitherThither.tsx
@@ -2,48 +2,48 @@
 <tileset version="1.10" tiledversion="1.11.0" name="WhitherThither" tilewidth="32" tileheight="32" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../Graphics/Rote.png" width="32" height="32"/>
+  <image source="../../Graphics/Rote.png" width="32" height="32"/>
  </tile>
  <tile id="2">
-  <image source="../Graphics/Marball.png" width="32" height="32"/>
+  <image source="../../Graphics/Marball.png" width="32" height="32"/>
  </tile>
  <tile id="5">
-  <image source="../Graphics/Down.png" width="32" height="32"/>
+  <image source="../../Graphics/Down.png" width="32" height="32"/>
  </tile>
  <tile id="6">
-  <image source="../Graphics/Right.png" width="32" height="32"/>
+  <image source="../../Graphics/Right.png" width="32" height="32"/>
  </tile>
  <tile id="7">
-  <image source="../Graphics/Up.png" width="32" height="32"/>
+  <image source="../../Graphics/Up.png" width="32" height="32"/>
  </tile>
  <tile id="10">
-  <image source="../Graphics/Left.png" width="32" height="32"/>
+  <image source="../../Graphics/Left.png" width="32" height="32"/>
  </tile>
  <tile id="11">
-  <image source="../Graphics/MarballLeft.png" width="32" height="32"/>
+  <image source="../../Graphics/MarballLeft.png" width="32" height="32"/>
  </tile>
  <tile id="12">
-  <image source="../Graphics/MarballRight.png" width="32" height="32"/>
+  <image source="../../Graphics/MarballRight.png" width="32" height="32"/>
  </tile>
  <tile id="13">
-  <image source="../Graphics/MarballUp.png" width="32" height="32"/>
+  <image source="../../Graphics/MarballUp.png" width="32" height="32"/>
  </tile>
  <tile id="14">
-  <image source="../Graphics/MarballDown.png" width="32" height="32"/>
+  <image source="../../Graphics/MarballDown.png" width="32" height="32"/>
  </tile>
  <tile id="15">
-  <image source="../Graphics/RMarballUp.png" width="32" height="32"/>
+  <image source="../../Graphics/RMarballUp.png" width="32" height="32"/>
  </tile>
  <tile id="16">
-  <image source="../Graphics/RMarballDown.png" width="32" height="32"/>
+  <image source="../../Graphics/RMarballDown.png" width="32" height="32"/>
  </tile>
  <tile id="17">
-  <image source="../Graphics/RDown.png" width="32" height="32"/>
+  <image source="../../Graphics/RDown.png" width="32" height="32"/>
  </tile>
  <tile id="18">
-  <image source="../Graphics/LDown.png" width="32" height="32"/>
+  <image source="../../Graphics/LDown.png" width="32" height="32"/>
  </tile>
  <tile id="19">
-  <image source="../Graphics/Half_Blue.png" width="32" height="32"/>
+  <image source="../../Graphics/Half_Blue.png" width="32" height="32"/>
  </tile>
 </tileset>
