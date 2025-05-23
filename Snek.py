9519535087c894ddea705b9ec05b3d2a472c0a3e
isaac <isaac.ocarrolldesmond@gmail.com>
2025-05-24 00:24:53 +0200

    Made Snek be better.

--- Snek.py
+++ Snek.py
@@ -3,6 +3,7 @@ pygame.init()
 from pygame.sprite import Sprite
 import sys
 import random
+from Colour_Picker import picks
 
 class Game:
     """Attempts a game"""
@@ -16,17 +17,17 @@ class Game:
         self.char = Snake(self)
         self.ticks = 0
         self.bite = Snack(self)
-        self.cols = []
-        for r in range(0,255,10):
-            for g in range(0,255,10):
-                for b in range(0,255,10):
-                    self.cols.append((r,g,b))
+        # self.cols = []
+        # for r in range(0,255,10):
+        #     for g in range(0,255,10):
+        #         for b in range(0,255,10):
+        #             self.cols.append((r,g,b))
 
     def run_game(self):
         while True:
             self.check_events()
             self.update_screen()
-            self.clock.tick(60)
+            self.clock.tick(12)
             self.ticks += 1
     
     def check_events(self):
@@ -35,23 +36,30 @@ class Game:
                 if event.key == pygame.K_q:
                     pygame.quit()
                     sys.exit()
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP and not self.char.down:
                     self.turn(0)
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_DOWN and not self.char.up:
                     self.turn(1)
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT and not self.char.right:
                     self.turn(2)
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT and not self.char.left:
                     self.turn(3)
                 if event.key == pygame.K_l:
                     self.char.lengthen()
+                    # self.char.reimage((200,200,200))
     
     def turn(self, ind):
         for cat in range(0,4):
             setattr(self.char, self.char.moves[cat], cat==ind)
 
     def update_screen(self):
-        self.screen.fill((self.cols[(self.ticks%len(self.cols))]))
+        # self.screen.fill((self.cols[(self.ticks%len(self.cols))]))
+        # self.screen.fill((23,99,127))
+        self.screen.fill((102,102,102))
+        try:
+            self.char.reimage(picks(self.char.rect.topleft, self.screen_width, self.char.tail*10))
+        except ValueError:
+            print(picks(self.char.rect.topleft, self.screen_width))
         self.char.update()
         self.bite.update()
         pygame.display.flip()
@@ -64,7 +72,8 @@ class Snake(Sprite):
         self.game:Game = game
         self.width = 20
         self.height = 20
-        self.image = pygame.image.load("Rot.png")
+        self.image = pygame.image.load("Skull.png")
+        self.image.set_colorkey((0,0,0))
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
         self.rect = self.image.get_rect()
         self.tail_0 = self.rect.copy()
@@ -73,23 +82,116 @@ class Snake(Sprite):
         self.up = False
         self.down = False
         self.moves = ["up", "down", "left", "right"]
-        self.tail = 100
+        self.tail = 1
         self.tails: list[pygame.rect.Rect] = [self.tail_0]
         self.fools = [(self.rect.x, self.rect.y)]
+        # self.image.set_palette((0,0,0))
+        self.image = pygame.PixelArray(self.image)
+        # self.image.replace((4,4,4),(102,102,102))
+        self.image = self.image.make_surface()
+        # self.image.fill((0,0,0))
+        self.images = []
+        self.breakdown()
+        self.og_images = []
+        for image in self.images:
+            self.og_images.append(image.copy())
+        self.cur = (4,4,4)
     
     def update(self):
         self.motion()
-        self.game.screen.blit(self.image, self.rect)
+        # self.game.screen.blit(self.image, self.rect)
+        self.skull()
+        for point in self.fools[1:]:
+            # self.game.screen.blit(self.image, point)
+            try:
+                self.game.screen.blit(self.images[self.adjutant(point)], point)
+            except TypeError:
+                pass
+        self.rattle()
         # for point in self.fools:
         #     self.game.screen.blit(self.image, point)
         # for tail in self.tails:
         #     self.game.screen.blit(self.image, tail)
-
     
+    def recolour(self, surface, colour, new):
+        surface = pygame.PixelArray(surface)
+        surface.replace((colour),(new))
+        surface = surface.make_surface()
+        return surface
+
+    def reimage(self, colour):
+        self.images = []
+        self.og_images.append(self.image)
+        for image in self.og_images.copy():
+            new = self.recolour(image, self.cur, colour)
+            new.set_colorkey((0,0,0))
+            self.images.append(new)
+        self.cur = colour
+
+    def adjutant(self, point):
+        self.fools.append(self.rect.topleft)
+        point_1 = self.fools[self.fools.index(point)-1]
+        point_2 = self.fools[self.fools.index(point)+1]
+        sector = [point_1, point_2]
+        if self.fools.index(point) == -1:
+            sector.append(self.rect.topleft)
+        self.fools.pop()
+        x,y = point[0],point[1]
+        if (x+20, y) in sector and (x, y+20) in sector:
+            return 0
+        elif (x+20, y) in sector and (x-20, y) in sector:
+            return 3
+        elif (x-20, y) in sector and (x, y+20) in sector:
+            return 6
+        elif (x, y+20) in sector and (x, y-20) in sector:
+            return 1
+        elif (x+20, y) in sector and (x, y-20) in sector:
+            return 2 
+        elif (x-20, y) in sector and (x, y-20) in sector:
+            return 8
+    
+    def rattle(self):
+        point = self.fools[0]
+        x,y = point[0], point[1]
+        poin2 = self.fools[1]
+        if (x,y-20) == poin2:
+            self.game.screen.blit(self.images[4], self.fools[0])
+        elif (x+20,y) == poin2:
+            self.game.screen.blit(pygame.transform.rotate(self.images[4],270), self.fools[0])
+        elif (x,y+20) == poin2:
+            self.game.screen.blit(pygame.transform.rotate(self.images[4],180), self.fools[0])
+        elif (x-20,y) == poin2:
+            self.game.screen.blit(pygame.transform.rotate(self.images[4],90), self.fools[0])
+
+    def skull(self):
+        point = self.rect.topleft
+        x,y = point[0], point[1]
+        poin2 = self.fools[-1]
+        if (x,y-20) == poin2:
+            self.game.screen.blit(pygame.transform.rotate(self.image,180), self.rect)
+        elif (x+20,y) == poin2:
+            self.game.screen.blit(pygame.transform.rotate(self.image,90), self.rect)
+        elif (x,y+20) == poin2:
+            self.game.screen.blit(pygame.transform.rotate(self.image,0), self.rect)
+        elif (x-20,y) == poin2:
+            self.game.screen.blit(pygame.transform.rotate(self.image,270), self.rect)
+
+    def breakdown(self):
+        image = pygame.image.load("Tails.png")
+        for cat in range(0,3):
+            for fish in range(0,3):
+                imager = pygame.surface.Surface((20,20))
+                imager.blit(image,(-cat*20,-fish*20))
+                imager.set_colorkey((0,0,0))
+                self.images.append(imager.copy())
+
     def lengthen(self):
         # setattr(self, f"tail_{self.tail+1}", getattr(self, f"tail_{self.tail}").copy())
         # self.tails.append(getattr(self, f"tail_{self.tail}"))
-        self.fools.append((self.rect.x, self.rect.y))
+        # self.fools.append((self.rect.x, self.rect.y))
+        self.fools.reverse()
+        self.fools.append(self.fools[0])
+        self.fools.reverse()
         self.tail += 1
     
     def trace(self):
@@ -111,12 +213,10 @@ class Snake(Sprite):
         #         self.tails.append(getattr(self, f"tail{tail}"))
         # self.trace()
         self.trace()
-        for point in self.fools:
-            self.game.screen.blit(self.image, point)
 
 
     def motion(self):
-        if self.game.ticks % 5 == 0:
+        if self.game.ticks % 1 == 0:
             self.trail()
             if self.up:
                 self.rect.y -= 20
@@ -129,6 +229,7 @@ class Snake(Sprite):
         if (self.rect.x,self.rect.y) in self.fools[:-2]:
             self.tail = len(self.fools[self.fools.index((self.rect.topleft)):])
             # self.tail = 0
+        self.lengthen()
 
 class Snack(Sprite):
     """Creates refreshments"""
